936959b13666f0ebfbfc64d30b5c8b1634464cb0
Denis Wolf <dear.mr.dw@gmail.com>
2013-11-11 02:52:29 +0200

    Sites model and controller

---
+++ api/models/Sites.js
@@ -0,0 +1,22 @@
+/**
+ * Sites
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+		site_id: 'string',
+		up_votes: 'integer',
+		down_votes: 'integer'
+  	
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+    
+  }
+
+};
